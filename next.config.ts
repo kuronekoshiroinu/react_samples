8ec4cbc48dd2ae7522fad6c03a72a1714041aac9
kuronekoshiroinu <ckenaco@gmail.com>
2026-01-04 22:54:39 -0400

    fix: allow cross origin

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {},
-  /* config options here */
+    experimental: {},
+    allowedDevOrigins: ['*'],
+    /* config options here */
 };
 
 export default nextConfig;
